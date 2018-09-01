50b06e5851cdc680a29b7eb6e8896a70336ff35f
varun dwarkani <varundwarkani1@gmail.com>
2018-09-01 23:38:06 +0530

    Splash Screen

---
+++ app/src/main/java/peri/periit/SplashScreen.java
@@ -0,0 +1,34 @@
+package peri.periit;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                    Intent loginIntent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(loginIntent);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+    }
+}
